970cf9ac0694d518c3c7ab2dd567b4656cbb39b4
lemon1499 <404997046@qq.com>
2019-12-19 18:00:49 +0800

    :lipstick:modify app.less and grid param

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,16 +7,16 @@ export default class Header extends React.Component{
     return (
       <div className='header'>
         <Row className='header-top'>
-          <Col span='24'>
+          <Col span={24}>
             <span>欢迎, yyl</span>
             <a>退出</a>
           </Col>
         </Row>
         <Row className='breadcrumb'>
-          <Col span='4' className='breadcrumb-title'>
+          <Col span={4} className='breadcrumb-title'>
             首页
           </Col>
-          <Col span='20' className='weather'>
+          <Col span={20} className='weather'>
             <span className='date'>2019-12-19</span>
             <span className='weather-detail'>晴转多云</span>
           </Col>
